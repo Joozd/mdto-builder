c4f16e11b4c58ed06104c34c7159dd9b7698d3e7
joozd <euroshopper@gmail.com>
2026-02-24 21:24:50 +0100

    Made MDTO backbone

--- src/main/kotlin/mdto/xmlwriters/MDTORoot.kt
+++ src/main/kotlin/mdto/xmlwriters/MDTORoot.kt
@@ -3,14 +3,40 @@ package nl.joozd.mdto.xmlwriters
 import nl.joozd.mdto.objects.Bestand
 import nl.joozd.mdto.objects.Informatieobject
 import nl.joozd.mdto.objects.MDTORoot
-import nl.joozd.mdto.objects.MdtoBaseClassContent
 import javax.xml.stream.XMLStreamWriter
 
 internal fun MDTORoot.emit(writer: XMLStreamWriter) {
-    writer.writeStartElement(MDTOXmlConstants.NS, MDTORoot.OBJECT_NAME)
-    when(content){
+    writer.writeStartDocument("UTF-8", "1.0")
+
+    // Default namespace instellen
+    writer.setDefaultNamespace(MDTOXmlConstants.NS)
+
+    writer.writeStartElement(
+        MDTOXmlConstants.NS,
+        MDTOXmlConstants.ROOT_ELEMENT
+    )
+
+    // xmlns="..."
+    writer.writeDefaultNamespace(MDTOXmlConstants.NS)
+
+    // xmlns:xsi="..."
+    writer.writeNamespace(
+        MDTOXmlConstants.XSI_PREFIX,
+        MDTOXmlConstants.XSI_NS
+    )
+
+    // xsi:schemaLocation="..."
+    writer.writeAttribute(
+        MDTOXmlConstants.XSI_NS,
+        "schemaLocation",
+        MDTOXmlConstants.SCHEMA_LOCATION
+    )
+
+    when (content) {
         is Bestand -> content.emit("bestand", writer)
         is Informatieobject -> content.emit("informatieobject", writer)
     }
+
     writer.writeEndElement()
+    writer.writeEndDocument()
 }
